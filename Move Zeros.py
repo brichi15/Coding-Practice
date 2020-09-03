4c53028bd6a7e1cf8b42537da172277913d170a4
Ho Woo Brian Chiu <chiuh1@mcmaster.ca>
2020-09-02 21:36:01 -0400

    more proper solution

--- Move Zeros.py
+++ Move Zeros.py
@@ -4,19 +4,19 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        zero_count = 0
         
-        i=0                     ##iterator
-        
+        zero_holder = 0                               
+        i = 0
         while i < len(nums):
+        
+            if nums[zero_holder] != 0:                  ## stop at first zero
+                zero_holder += 1
+                
+            if nums[i] != 0 and i > zero_holder:        ##swap
+                
+                nums[zero_holder] = nums[i]
+                nums[i] = 0
+        
             
-            if nums[i] == 0:
-                nums.pop(i)
-                zero_count += 1
-                
-                
-            else:
-                i += 1
-                
-        nums += [0]*zero_count
-            
+            i +=1
+                        
