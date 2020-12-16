f054996cedbb5cf79607b1bacd426da01220f7d9
brichi15 <b_rian_chiu@hotmail.com>
2020-12-16 13:47:09 -0500

    2020-12-16 TIMEOUT 59/69

---
+++ house-robber.py
@@ -0,0 +1,27 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        if not nums: return 0
+        
+        
+        visited = dict()
+        def dfs(ind):
+            
+            total = 0
+        
+            if ind >= len(nums): return total
+
+            if ind+2 in visited: p1 = visited[ind+2]
+            else: p1 = dfs(ind+2)
+                
+            if ind+3 in visited: p2 = visited[ind+3]
+            else: p2 = dfs(ind+3)
+        
+            return nums[ind] + max(p1,p2)
+                
+                
+        first = dfs(0)
+        second = dfs(1)
+        
+        
+        return max(first,second)
