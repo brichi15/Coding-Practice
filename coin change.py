9ed1c3a32ca35f1fbf4f43d5182dfaa725aa08f8
brichi15 <b_rian_chiu@hotmail.com>
2020-12-17 20:05:07 -0500

    add memoized solution

--- coin change.py
+++ coin change.py
@@ -1,3 +1,38 @@
+##memoized solution
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        if amount == 0:
+            return amount
+        
+        visited = {}        
+        
+        def dfs(cur_sum):
+            
+            if cur_sum > amount:
+                return amount+1
+            elif cur_sum == amount:
+                return 0
+            
+            ans = amount+1
+            
+            for coin in coins:
+                
+                if cur_sum+coin in visited:
+                    temp = visited[cur_sum+coin]
+                else:
+                    temp = dfs(cur_sum+coin)
+                    visited[cur_sum+coin] = temp
+                    
+                ans = min(ans, temp+1)
+                    
+            return ans
+        
+        res = dfs(0)
+        
+        return res if res <= amount else -1
+
+## tabulated solution
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
